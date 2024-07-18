27742564ecdae41c0b86f74ff5ab2f29f5b19564
kelvinehiz <kelvinehiz@gmail.com>
2024-07-18 01:37:46 +0000

    redis advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
 """
-web cache and tracker
+Web cache and tracker
 """
 import requests
 import redis
 from functools import wraps
 
-store = redis.Redis()
+class WebCache:
+    def __init__(self):
+        self.store = redis.Redis()
 
+    def count_url_access(self, method):
+        """ Decorator counting how many times a URL is accessed """
+        @wraps(method)
+        def wrapper(url):
+            cached_key = "cached:" + url
+            cached_data = self.store.get(cached_key)
+            if cached_data:
+                return cached_data.decode("utf-8")
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+            count_key = "count:" + url
+            html = method(url)
 
-        count_key = "count:" + url
-        html = method(url)
+            self.store.incr(count_key)
+            self.store.set(cached_key, html)
+            self.store.expire(cached_key, 10)
+            return html
+        return wrapper
 
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+    @count_url_access
+    def get_page(self, url: str) -> str:
+        """ Returns HTML content of a url """
+        res = requests.get(url)
+        return res.text
 
-
-@count_url_access
-def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+if __name__ == "__main__":
+    web_cache = WebCache()
+    url = "http://slowwly.robertomurray.co.uk"
+    print(web_cache.get_page(url))
